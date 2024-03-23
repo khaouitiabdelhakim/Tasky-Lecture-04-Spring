8488c369a20f5381b564f51b148228dd361a1995
Ahmed Idrissi <83851898+ahmedidrissi@users.noreply.github.com>
2024-03-23 22:19:41 +0000

    delete

--- src/main/java/com/abdelhakim/Tasky/service/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abdelhakim.Tasky.service;
-
-import com.abdelhakim.Tasky.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
-
